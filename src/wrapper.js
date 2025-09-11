8693de174238537e9585de246896b74a75baf919
DevelopedBy-Siva <developedby.siva@gmail.com>
2025-09-11 15:25:40 -0700

    Implement submit

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,14 +1,18 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 import Rules from "./rules";
 import Loader from "./Loader";
 import { DATA_JSON_PATH } from "./constants";
-import axios from "axios";
+import ReactFocusLock from "react-focus-lock";
 
 export default function Wrapper() {
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [submissionSuccess, setSubmissionSuccess] = useState(false);
+  const [submissionError, setSubmissionError] = useState(false);
 
   useEffect(() => {
     async function getData() {
@@ -27,8 +31,25 @@ export default function Wrapper() {
     getData();
   }, []);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsSubmit(true);
+    setSubmissionError(false);
+    setSubmissionSuccess(false);
+    const base64Content = btoa(JSON.stringify(data, null, 2));
+
+    try {
+      await axios.post("/api/newfile", {
+        path: "data/data.json",
+        content: base64Content,
+        commitMessage: "Update data.json",
+      });
+      setSubmissionSuccess(true);
+    } catch (_) {
+      setSubmissionError(true);
+    } finally {
+      setIsSubmit(false);
+    }
   };
 
   const handleDownload = () => {
@@ -43,35 +64,58 @@ export default function Wrapper() {
     URL.revokeObjectURL(url);
   };
 
-  return isLoading ? (
-    <Loader />
-  ) : error ? (
-    <p className="error-note">
-      Oops! Something went wrong while retrieving the data. Please refresh the
-      page or try again later.
-    </p>
-  ) : (
-    <div className="wrapper">
-      <p className="header-msg">
-        <u>Note</u>: After making changes, it may take up to 3–5 minutes for
-        updates to appear on the portfolio website.
-      </p>
-      <form onSubmit={handleSubmit}>
-        {Object.keys(data).map((item, idx) => (
-          <div className="input-block" key={idx}>
-            <Rules input_name={item} data={data} setData={setData} />
-          </div>
-        ))}
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <p className="error-note">
+          Oops! Something went wrong while retrieving the data. Please refresh
+          the page or try again later.
+        </p>
+      ) : (
+        <div className="wrapper">
+          <p className="header-msg">
+            <u>Note</u>: After making changes, it may take up to 3–5 minutes for
+            updates to appear on the portfolio website.
+          </p>
+          <form onSubmit={handleSubmit}>
+            {Object.keys(data).map((item, idx) => (
+              <div className="input-block" key={idx}>
+                <Rules input_name={item} data={data} setData={setData} />
+              </div>
+            ))}
 
-        <div className="submit">
-          <button className="submit-btn" type="submit">
-            Submit
-          </button>
-          <button onClick={handleDownload} className="json" type="button">
-            Download JSON
-          </button>
+            <div className="submit">
+              <button className="submit-btn" type="submit">
+                Submit
+              </button>
+              <button onClick={handleDownload} className="json" type="button">
+                Download JSON
+              </button>
+              {submissionSuccess && (
+                <p className="submit-success">Data is successfully updated</p>
+              )}
+              {submissionError && (
+                <p className="submit-error">
+                  Oops. Failed to update data. Try again or later
+                </p>
+              )}
+            </div>
+          </form>
         </div>
-      </form>
-    </div>
+      )}
+      {isSubmit && <SubmissionProgress />}
+    </>
+  );
+}
+
+function SubmissionProgress() {
+  return (
+    <ReactFocusLock>
+      <div className="submission-progress">
+        <button style={{ opacity: 0 }}></button>
+      </div>
+    </ReactFocusLock>
   );
 }
