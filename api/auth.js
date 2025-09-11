4d5fc54c11c1996a957823387c20138bd8176cc3
DevelopedBy-Siva <developedby.siva@gmail.com>
2025-09-11 09:39:35 -0700

    Fix auth

--- api/auth.js
+++ api/auth.js
@@ -2,6 +2,6 @@ export default function handler(req, res) {
   const { username, password } = req.body;
 
   if (process.env.USERNAME === username && process.env.PASSWORD === password)
-    return res.status(204);
+    return res.status(200).json({ message: "Authentication Successful" });
   return res.status(401).json({ message: "Invalid credentials" });
 }
