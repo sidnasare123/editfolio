eb45dd418a4af28b6dcb71ebd90239fd52ef8405
sidnasare123 <sidnasare2608@gmail.com>
2025-09-12 20:34:48 -0700

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const JWT_TOKEN_KEY = "EDITFOLIO_JWT_TOKEN";
 export const DATA_JSON_PATH =
-  "https://raw.githubusercontent.com/prempritam888/portfolio-assets/refs/heads/main/data/data.json";
+  "https://raw.githubusercontent.com/sidnasare123/portfolio-assets/refs/heads/main/data/data.json";
 export const PROFILE_IMAGE_PATH =
-  "https://github.com/prempritam888/portfolio-assets/blob/main/profile/";
+  "https://github.com/sidnasare123/portfolio-assets/blob/main/profile/";
 export const RESUME_PATH =
-  "https://github.com/prempritam888/portfolio-assets/blob/main/resume/";
+  "https://github.com/sidnasare123/portfolio-assets/blob/main/resume/";
